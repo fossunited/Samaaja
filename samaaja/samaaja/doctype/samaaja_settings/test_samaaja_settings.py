97f2e4368f5fb449e9ef70ff38ca23c035bf80ae
Abhinav Raut <abhinavrautcs@gmail.com>
2023-03-11 18:39:56 +0530

    feat: doctype to store common lat long precision

---
+++ samaaja/samaaja/doctype/samaaja_settings/test_samaaja_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, FOSS United and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestSamaajaSettings(FrappeTestCase):
+	pass
